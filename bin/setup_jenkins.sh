12de8e9934ac509bda8e6497f9771dfeaa94ec28
dvukasovic <denis.vukasovic@comping.hr>
2019-01-28 08:37:15 +0000

    Initial

---
+++ bin/setup_jenkins.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Setup Jenkins Project
+if [ "$#" -ne 3 ]; then
+    echo "Usage:"
+    echo "  $0 GUID REPO CLUSTER"
+    echo "  Example: $0 wkha https://github.com/redhat-gpte-devopsautomation/advdev_homework_template.git na311.openshift.opentlc.com"
+    exit 1
+fi
+
+GUID=$1
+REPO=$2
+CLUSTER=$3
+echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
+
+# Set up Jenkins with sufficient resources
+oc new-app jenkins-persistent --name=jenkins -p JENKINS_SERVICE_NAME=jenkins -p MEMORY_LIMIT=2Gi -p VOLUME_CAPACITY=5Gi -p NAMESPACE=openshift -p DISABLE_ADMINISTRATIVE_MONITORS=true -p ENABLE_OAUTH=true -n ${GUID}-jenkins
+oc set resources dc jenkins --limits=cpu=2,memory=2Gi --requests=cpu=500m,memory=1Gi -n ${GUID}-jenkins
+
+# Create custom agent container image with skopeo
+oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n USER root\nRUN yum -y install skopeo && yum clean all\n USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+
+# Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
+oc create -n "${GUID}-jenkins" -f - <<EOF
+apiVersion: v1
+items:
+- kind: "BuildConfig"
+  apiVersion: "v1"
+  metadata:
+    name: "tasks-pipeline"
+  spec:
+    source:
+      contextDir: "openshift-tasks"
+      type: "Git"
+      git:
+        reg: "dev"
+        uri: "${REPO}"
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfilePath: Jenkinsfile
+kind: List
+metadata: []
+EOF
+
+# Make sure that Jenkins is fully up and running before proceeding!
+while : ; do
+  echo "Checking if Jenkins is Ready..."
+  AVAILABLE_REPLICAS="$(oc get dc jenkins -n ${GUID}-jenkins -o=jsonpath='{.status.availableReplicas}')"
+  if [[ "$AVAILABLE_REPLICAS" == "1" ]]; then
+    echo "...Yes. Jenkins is ready."
+    break
+  fi
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
